9f049904982e26c97ab58d8b862b40692aa69fbb
Ruben Durango <ruben.durango@haceb.com>
2025-04-07 12:04:35 -0500

    Agregar modelo CustomerCreate y actualizar la ruta para crear clientes en la aplicación FastAPI

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 from fastapi import FastAPI
-from models import Customer, Transaction, Invoice
+from models import Customer,CustomerCreate, Transaction, Invoice
 
 app = FastAPI()
 
@@ -28,10 +28,16 @@ async def time(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
-@app.post("/customers")
-async def create_customer(customer_data: Customer):
+current_id: int = 0
+db_customers: list[Customer] = []
+
+@app.post("/customers", response_model=Customer)
+async def create_customer(customer_data: CustomerCreate):
+    customer = Customer.model_validate(customer_data.model_dump())
+    customer.id = len(db_customers) + 1
+    db_customers.append(customer)
     # Simulate a database operation
-    return customer_data
+    return customer
 
 @app.post("/transactions")
 async def create_transaction(transaction_data: Transaction):
