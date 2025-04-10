cb551c15160f8a6024aa9f6ad046977ebfd1ce2f
Ruben Durango <ruben.durango@haceb.com>
2025-04-10 14:33:24 -0500

    Refactor código para mejorar la estructura de modelos y corregir errores tipográficos en la definición de campos

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import zoneinfo
 from datetime import datetime
 
-
 from fastapi import FastAPI
-from models import Customer,CustomerCreate, Transaction, Invoice
+from models import Customer, CustomerCreate, Transaction, Invoice
+from db import SessionDep, create_all_tables
 
-app = FastAPI()
+app = FastAPI(lifespan=create_all_tables)
+
 
 @app.get("/")
 async def root():
@@ -28,27 +29,30 @@ async def time(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
-current_id: int = 0
+
 db_customers: list[Customer] = []
 
+
 @app.post("/customers", response_model=Customer)
-async def create_customer(customer_data: CustomerCreate):
+async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump())
-    customer.id = len(db_customers) + 1
+    # Ausmiendo que hace base de datos
+    customer.id = len(db_customers)
     db_customers.append(customer)
-    # Simulate a database operation
     return customer
 
+
 @app.get("/customers", response_model=list[Customer])
-async def list_customers():
+async def list_customer():
     return db_customers
 
+
 @app.post("/transactions")
-async def create_transaction(transaction_data: Transaction):
-    # Simulate a database operation
+async def create_transation(transaction_data: Transaction):
     return transaction_data
 
-@app.post("/invoices")
+
+@app.post("/invoices", response_model=Invoice)
 async def create_invoice(invoice_data: Invoice):
-    # Simulate a database operation
+    breakpoint()
     return invoice_data
--- models.py
+++ models.py
@@ -1,29 +1,34 @@
-
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
-class CustomerBase(BaseModel):
-    name: str
-    description: str | None
-    email: str
-    age: int
-    
+
+class CustomerBase(SQLModel):
+    name: str = Field(default=None)
+    description: str | None = Field(default=None)
+    email: str = Field(default=None)
+    age: int = Field(default=None)
+
+
 class CustomerCreate(CustomerBase):
     pass
 
-class Customer(CustomerBase):
-    id: int | None = None
-    
+
+class Customer(CustomerBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
 class Transaction(BaseModel):
     id: int
-    amount: int
+    ammount: int
     description: str
-    
+
+
 class Invoice(BaseModel):
     id: int
     customer: Customer
     transactions: list[Transaction]
     total: int
-    
+
     @property
-    def amount_total(self) -> int:
-        return sum(transaction.amount for transaction in self.transactions)
+    def ammount_total(self):
+        return sum(transaction.ammount for transaction in self.transactions)
